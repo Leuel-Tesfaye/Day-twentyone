b4dbf29fc7809c2749a811082133fcf0d2a02629
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-07-05 15:36:13 +0300

    exercise dom

---
+++ exercise.js
@@ -0,0 +1,11 @@
+// Exercise 1 : 
+// 1.  Create an index.html file and put four p elements as above: Get the first paragraph by using document.querySelector(tagname) and tag name
+
+const firstPara = document.querySelector('p')
+console.log(firstPara)
+
+// 2. Get each of the the paragraph using document.querySelector('#id') and by their id
+const allParagraph = document.getElementById('para-graph')
+console.log(allParagraph)
+
+
